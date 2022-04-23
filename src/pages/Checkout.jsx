abec138a22ecd1e33b01ee96ff531d6d46fe6082
aguirremx <agsaro7@gmail.com>
2022-04-22 19:39:33 -0500

    checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import OrderItem from '@components/OrderItem';
-import '@styles/Checkout.scss';
+import React, { useContext } from "react";
+import AppContext from '@context/AppContext'
+import OrderItem from "@components/OrderItem";
+import "@styles/Checkout.scss";
 
 const Checkout = () => {
-	return (
-		<div className="Checkout">
-			<div className="Checkout-container">
-				<h1 className="title">My order</h1>
-				<div className="Checkout-content">
-					<div className="order">
-						<p>
-							<span>03.25.21</span>
-							<span>6 articles</span>
-						</p>
-						<p>$560.00</p>
-					</div>
-				</div>
-				{/* <OrderItem /> */}
-			</div>
-		</div>
-	);
-}
+  const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
+  return (
+    <div className="Checkout">
+      <div className="Checkout-container">
+        <h1 className="title">My order</h1>
+        <div className="Checkout-content">
+          <div className="order">
+            <p>
+              <span>03.25.21</span>
+              <span>{state.cart.length} articulos</span>
+            </p>
+            <p>${sumTotal()}</p>
+          </div>
+        </div>
+        {state.cart.map((product) => (
+          <OrderItem product={product} key={`orderItem-${product.id}`} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Checkout;
